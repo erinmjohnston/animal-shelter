79ddda50430eca97a15f77f5378e44511e9d7bb7
Erin Johnston <emj3fg@virginia.edu>
2020-02-19 19:35:41 -0500

    made the things spaced right

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import {MatButtonModule, MatIconModule, MatSelectModule, MatSidenavModule} from '@angular/material';
@@ -11,6 +12,7 @@ import {MatButtonModule, MatIconModule, MatSelectModule, MatSidenavModule} from 
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
